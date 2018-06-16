21c390fbc7ec1a70b8ed91c19b5b46fa8b187c42
schun92 <sarahsandiferchun@gmail.com>
2018-06-15 18:31:14 -0700

    react-intro - Sarah Chun

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const user ={
+    name: 'Sarah',
+    luckyNumber: luckyNumber()
+};
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
+
+
+function luckyNumber(){
+    return Math.floor(Math.random()*1000)+1;
+};
+
+function greeting(person){
+   return <div className="container">
+   <h1>Hello {person.name}</h1>
+   <h2 className="text-muted">The lucky number is: {person.luckyNumber}</h2>
+   </div>
+};
+
+
+
