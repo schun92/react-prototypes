a2a6d824db8ad07928acfb7da66d646a227c2e76
schun92 <sarahsandiferchun@gmail.com>
2018-07-03 16:45:22 -0700

    completed building clock with react and redux

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,12 @@
+import React from 'react';
+import Clock from './clock';
 
 
+export default () => {
+    return (
+        <div className="container">
+            <h1 className="text-center">React Redux</h1>
+            <Clock />
+        </div>
+    )
+}
